4ab053a804fce93ebbf80d8450ce3aa02029b464
elijah <cuiyeqing960904@gmail.com>
2023-10-19 18:34:15 -0400

    login_code verification

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -24,10 +24,29 @@ class LoginController extends Controller
         }
 
         $user->notify(new LoginNeedsVerification());
+
+        return response()->json(['message' => 'Text message notification sent.']);
     }
 
     public function verify(Request $request)
     {
+        $request->validate([
+            'phone' => 'required|numeric|min:10',
+            'login_code' => 'required|numeric|between:111111,999999'
+        ]);
 
+        $user = User::where('phone', $request->phone)
+            ->where('login_code', $request->login_code)
+            ->first();
+
+        if ($user) {
+            $user->update([
+                'login_code' => null
+            ]);
+
+            return $user->createToken($request->login_code)->plainTextToken;
+        }
+
+        return response()->json(['message' => 'Invalid verification code.'], 401);
     }
 }
