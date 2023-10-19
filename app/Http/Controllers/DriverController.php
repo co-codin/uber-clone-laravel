0fc592d2547cbeb97a2d73ff63d1ab5284e9927c
elijah <cuiyeqing960904@gmail.com>
2023-10-19 18:11:37 -0400

    controllers setup

---
+++ app/Http/Controllers/DriverController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+// lazy to build form request , need to finish it fast
+class DriverController extends Controller
+{
+    public function show(Request $request)
+    {
+
+    }
+
+    public function update(Request $request)
+    {
+
+    }
+}
---
+++ app/Http/Controllers/TripController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+// lazy to build form request , need to finish it fast
+class TripController extends Controller
+{
+    public function store(Request $request)
+    {
+
+    }
+
+    public function show(Request $request)
+    {
+
+    }
+
+    public function accept(Request $request)
+    {
+
+    }
+
+    public function start(Request $request)
+    {
+
+    }
+}
