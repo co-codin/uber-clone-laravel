3605fcd0ceda2af8e98afcb0813e421bb1a8d7fa
elijah <cuiyeqing960904@gmail.com>
2023-10-19 18:24:25 -0400

    building twillio notification

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 // lazy to build form request , need to finish it fast
@@ -9,7 +10,19 @@ class LoginController extends Controller
 {
     public function submit(Request $request)
     {
+        $request->validate([
+            'phone' => 'required|numeric|min:10'
+        ]);
 
+        $user = User::query()->firstOrCreate([
+            'phone' => $request->phone
+        ]);
+
+        if (!$user) {
+            return response()->json(['message' => 'Could not process a user with that phone number.'], 401);
+        }
+
+        $user->notify();
     }
 
     public function verify(Request $request)
